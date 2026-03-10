b2029c79af0ce6fff4b6c8a804a3fdbc35d3ae8e
iWorld-y <caima12138@foxmail.com>
2026-03-10 15:45:13 +0800

    feat(server): setup Kratos HTTP server

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -1,9 +1,47 @@
 package main
 
 import (
+	"context"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/config"
+	"github.com/go-kratos/kratos/v2/config/file"
+	"github.com/go-kratos/kratos/v2/transport/http"
 )
 
 func main() {
-	log.Println("SnapCard server starting...")
+	c := config.New(
+		config.WithSource(
+			file.NewSource("configs/config.yaml"),
+		),
+	)
+	if err := c.Load(); err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	httpSrv := http.NewServer(
+		http.Address(":8000"),
+	)
+
+	app := kratos.New(
+		kratos.Server(httpSrv),
+	)
+
+	go func() {
+		if err := app.Run(); err != nil {
+			log.Printf("server stopped: %v", err)
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+	<-stop
+
+	if err := app.Stop(context.Background()); err != nil {
+		log.Printf("failed to stop app: %v", err)
+	}
 }
